50424fcbfdbb87f93670e787a6e724f35674bb22
AlexandroDRK <alexsandro060302@gmail.com>
2023-05-11 17:33:22 -0300

    Create a symbol automaton and modify main logics

--- lexico/automatos.rb
+++ lexico/automatos.rb
@@ -1,7 +1,6 @@
 filename = ARGV[0]
 file = File.open(filename, "r")
 entrada = file.read
-$token = ""
 
 
 def automato_numerico(entrada)
@@ -21,48 +20,48 @@ def automato_numerico(entrada)
         when /[[:digit:]]/
             if estado_a == 1
                 estado_a = estados[1]["d"]
-                $token << i
+                
             elsif estado_a == 2
                 estado_a = estados[2]["d"]
-                $token << i
+                
             elsif estado_a == 3
                 estado_a = estados[3]["d"]
-                $token << i
+                
             elsif estado_a == 4
                 estado_a = estados[4]["d"]
-                $token << i
+                
             elsif estado_a == 5
                 estado_a = estados[5]["d"]
-                $token << i
+                
             else
                 estado_a = -1
             end
         when /[-]/
             if estado_a == 1
                 estado_a = estados[1]["-"]
-                $token << i
+                
             else
                 estado_a = -1
             end
         when /[,]/
             if estado_a == 2
                 estado_a = estados[2][","]
-                $token << i
+                
             else
                 estado_a = -1
             end
         else
             break
         end
-    end
+		end
 
-    if estado_a == 2 or estado_a == 4
-        puts "Cadeia reconhecida"
-    else
-        puts "Cadeia não reconhecida"
+		estados_finais = [2,4]
+		if estados_finais.include?(estado_a)
+			puts "Cadeia reconhecida"
+		else
+			puts "Cadeia não reconhecida"
+		end
     end
-    
-end
 
 def automato_identificador(entrada)
     estados = {
@@ -77,65 +76,127 @@ def automato_identificador(entrada)
     entrada.each_char do |i|
         puts estado_a
         case i
-        when /[[:alpha:]]/
+        when /[[a-zA-Z]]/
             if estado_a == 1
                 estado_a = estados[1]["l/L"]
-                $token << i
-            else
-                estado_a = -1
+                
             end 
-        when (/[[:alpha:]]/ || /[[:digit:]]/)
+        when /[a-zA-Z0-9]/
             if estado_a == 2 
                 estado_a = estados[2]["l/L/d"]
-                $token << i 
+                 
             elsif estado_a == 3
                 estado_a = estados[3]["l/L/d"]
-                $token << i
+                
             elsif estado_a == 4
                 estado_a = estados[4]["l/L/d"]
-                $token << i 
-            else
-                estado_a = -1
+                 
             end
-        when /[_]/ || /[~]/
+        when /[_~]/
             if estado_a == 2
                 puts estado_a
                 estado_a = estados[2]["_/~"]
-                $token << i
-            else
-                estado_a = -1
+                
             end  
-        else
-            break
         end
     end
 
-    if estado_a == 2 or estado_a == 3
+    estados_finais = [2,3]
+    if estados_finais.include?(estado_a)
         puts "Cadeia reconhecida"
     else
         puts "Cadeia não reconhecida"
     end
 end
 
-def automato_simbolos(entrada):
+
+def automato_simbolos(entrada)
     estados = {
-        1=> {";"=> 2,","=> 2,"."=> 2,"*"=> 2, "/"=> 2, "@"=> 2,"("=> 2, ")"=> 2,"{"=> 2,"}"=> 2,"="=> 6, "+"=> 7, ":"=> 3, ">"=> 4, "<"=> 5, ":"=> 6, "-"=> 8},
+        1=> {[";", ",", ".", "*", "/", "@", "(", ")", "{", "}"]=> 2, "<" => 5,":"=> 3, ">" => 4, "=" => 6, "+" => 7, "-" => 8},
         3=> {"="=> 6},
         4=> {"="=> 6},
-        5=> {"</="=> 6}, 
-        8=> {"-"=> 6},
-    }
+        5=> {[">","="]=> 6}, 
+		  7=> {"+"=> 2}, 
+        8=> {"-"=> 6}
+    }  
 
+	 estado1 = [";", ",", ".", "*", "/", "@", "(", ")", "{", "}"]
+	 estado5 = [">","="]
+
+
+    estado_a = 1
 
     entrada.each_char do |i|
-        puts estado_a
-        case i
-        when /[;]/
-            if  
+      puts estado_a
+      case i
+		when *estado1
+			if estado_a == 1
+				estado_a = estados[1][estados[1].keys.first]
+			else
+				estado_a = -1
+			end
+		when /[:]/
+			if estado_a == 1
+				estado_a = estados[1][":"]
+			else
+				estado_a = -1
+			end
+		when /[=]/
+			if estado_a == 1
+				estado_a = estados[1]["="]
+			elsif estado_a == 3
+				estado_a = estados[3]["="]
+			elsif estado_a == 4
+				estado_a = estados[4]["="]
+			elsif estado_a == 5
+				estado_a = estados[5][estados[5].keys.first]
+			elsif estado_a == 7
+				estado_a = estados[7]["="]
+			elsif estado_a == 8
+				estado_a = estados[8]["="]
+			else
+				estado_a = -1
+			end
+		when /[>]/
+			if estado_a == 1
+				estado_a = estados[1][">"]
+			elsif estado_a == 5
+				estado_a = estados[5][estados[5].keys.first]
+			else
+				estado_a = -1
+			end
+		when /[<]/	
+			if estado_a == 1
+				estado_a = estados[1]["<"]
+			else
+				estado_a = -1
+			end
+		when /[+]/
+			if estado_a == 1
+				estado_a = estados[1]["+"]
+			elsif estado_a == 7
+				estado_a = estados[7]["+"]
+			else
+				estado_a = -1
+			end
+		when /[-]/
+			if estado_a == 1
+				estado_a = estados[1]["-"]
+			elsif estado_a == 8
+				estado_a = estados[8]["-"]
+			else
+				estado_a = -1
+			end
+		else
+			break
+		end 
+    end 
+
+	 estados_finais = [2,3,4,5,6,7,8]
+    if estados_finais.include?(estado_a)
+        puts "Cadeia reconhecida"
+    else
+        puts "Cadeia não reconhecida"
+    end
 end
-
-
-
-automato_identificador(entrada) 
-
-puts $token
+automato_numerico(entrada) 
